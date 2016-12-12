f93b85c993ee93e93fa12c08f9bab598a2263c6d
Thieu Le Quang <quangthieuagu@gmail.com>
2016-12-13 00:08:02 +0700

    add new source code

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-var elixir = require('laravel-elixir');
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue-2');
 
 /*
  |--------------------------------------------------------------------------
@@ -6,29 +8,12 @@ var elixir = require('laravel-elixir');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application as well as publishing vendor resources.
  |
  */
 
-elixir(function(mix) {
-    mix.less('app.less');
-    mix.copy('bower_components/bootstrap/dist/fonts', 'public/assets/fonts');
-   	mix.copy('bower_components/font-awesome/fonts', 'public/assets/fonts');
-   	mix.styles([
-        'bower_components/bootstrap/dist/css/bootstrap.css',
-        'bower_components/fontawesome/css/font-awesome.css',
-        'resources/css/sb-admin-2.css',
-        'resources/css/timeline.css'
-    ], 'public/assets/stylesheets/styles.css', './');
-    mix.scripts([
-        'bower_components/jquery/dist/jquery.js',
-        'bower_components/bootstrap/dist/js/bootstrap.js',
-        'bower_components/Chart.js/Chart.js',
-        'bower_components/metisMenu/dist/metisMenu.js',
-        'resources/js/sb-admin-2.js',
-        'resources/js/frontend.js'
-    ], 'public/assets/scripts/frontend.js', './');
+elixir((mix) => {
+    mix.sass('app.scss')
+       .webpack('app.js');
 });
-
-
---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['middleware' => 'auth'], function () {
+
+    /**** GET ***/
+    // users
+    Route::get('/', 'WelcomeController@index');
+    Route::get('/dash', 'DashboardController@index');
+
+    Route::get('/logout',[
+        'middleware' => 'role:public',
+        'uses' => 'UserController@logout',
+    ]);
+    Route::get('user/profile',[
+        'middleware' => 'role:public',
+        'uses' => 'UserController@getUserProfile',
+    ]);
+
+    Route::get('/permission', 'UserController@listPermission');
+    Route::get('/permission-add', 'UserController@addPermission');
+    Route::get('/permission-edit/{permissionId}', 'UserController@editPermission');
+    Route::get('/permission-remove/{permissionId}', 'UserController@removePermission');
+    Route::get('/permission-add-user-group', [
+        'middleware' => 'role:QLNQ-Update',
+        'uses' => 'UserController@addUserGroupPermission',
+    ]);
+
+});
+
+//GUEST ROLE
+Route::group(['middleware' => 'guest'], function () {
+
+    /**
+     * GET request
+     */
+    Route::get('/login', 'UserController@login');
+
+
+    Route::get('/error503', 'ErrorController@error503');
+    Route::get('/forgot-pass-message', 'UserController@forgotPasswordMessage');
+    Route::get('/renewpass/{token}', 'UserController@renewPassword');
+    Route::get('/forgot-password', 'UserController@forgotPassword');
+    Route::get('/captcha', 'UserController@captchamage');
+    Route::get('/confirm-email/{token}', 'UserController@postConfirmEmail');
+    /**
+     * POST request
+     */
+    Route::post('/login', 'UserController@doLogin');
+    Route::post('/post-forgot-password', 'UserController@postForgotPassword');
+    Route::post('/renewpass/{token}', 'UserController@renewPassword');
+    Route::post('/postrenewpass/{token}', 'UserController@postRenewPassword');
+    Route::post('/post-register-agency', 'AgencyController@postRegisterAgency');
+
+});
