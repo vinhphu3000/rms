2bd85fadf631c0f92bc0aa80fea519008417b8e1
Thieu Le Quang <quangthieuagu@gmail.com>
2016-12-19 17:52:22 +0700

    add new database schema

--- resources/views/widgets/menu.blade.php
+++ resources/views/widgets/menu.blade.php
@@ -1,6 +1,5 @@
 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 	<div class="menu_section">
-		<h3>&nbsp;</h3>
 		<ul class="nav side-menu">
 			<li {{ (Request::is('dash') ? 'class="active"' : '') }} ><a href="{{ url ('user') }}"><i class="fa fa-user"></i> User </a></li>
 			<li {{ (Request::is('employee') ? 'class="active"' : '') }} ><a href="{{ url ('employee') }}"><i class="fa fa-users"></i> Employee</a></li>
