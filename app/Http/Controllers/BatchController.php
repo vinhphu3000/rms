6cc119e682cc73ce0a80ec7e118757cd136c7874
Thieu Le Quang <quangthieuagu@gmail.com>
2017-03-19 19:38:26 +0700

    notification (70%)

---
+++ app/Http/Controllers/BatchController.php
@@ -0,0 +1,50 @@
+<?php namespace App\Http\Controllers;
+
+use Request;
+use Session;
+use Input;
+use App\Http\Controllers\Controller as BaseController;
+
+
+class BatchController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Booking Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handle all process related to project like list, add new , assign resource
+	|
+	*/
+
+    protected  $user;
+
+    /**
+     * Constructor function
+     * Set current user information
+     */
+    public function __construct() {
+        $this->middleware(function ($request, $next) {
+            $this->user = \App\Authentication\Service::getAuthInfo();
+            $notification = Notification::where('send_to', $this->user->id)->get()->take(5);
+            $count_notify = Notification::where('send_to', $this->user->id)->where('status_seen', 0)->count();
+            view()->share('my', $this->user);
+            view()->share('notification', $notification);
+            view()->share('count_notify', $count_notify);
+            return $next($request);
+        });
+
+    }
+
+    /**
+     *  scan message
+     */
+    public function scanMessage()
+    {
+        $notification_service = new \App\Notification\Service();
+        $notification_service->scanForMessage($this->user_id);
+
+    }
+
+
+}
